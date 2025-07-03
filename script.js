b2e574f7a80efa52318a2400d5eefa5bf8c73ee6
viswa027 <127424946+viswa027@users.noreply.github.com>
2025-07-03 13:33:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-
 const newsList = document.getElementById('news-list');
 const chatBox = document.getElementById('chat-box');
 const qaLog = document.getElementById('qa-log');
 const newsTitle = document.getElementById('news-title');
 const questionInput = document.getElementById('question');
 
-fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=7cba2a33d3ea4342b5edfa51cf802319')
+fetch('https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=7cba2a33d3ea4342b5edfa51cf802319')
   .then(res => res.json())
   .then(data => {
     newsList.innerHTML = '';
-    data.articles.forEach(article => {
-      const div = document.createElement('div');
-      div.className = 'news-card';
-      div.innerText = article.title;
-      div.onclick = () => openChat(article.title);
-      newsList.appendChild(div);
-    });
+    if (data.articles.length === 0) {
+      newsList.innerHTML = '<p>No news articles found.</p>';
+    } else {
+      data.articles.forEach(article => {
+        const div = document.createElement('div');
+        div.className = 'news-card';
+        div.innerText = article.title;
+        div.onclick = () => openChat(article.title);
+        newsList.appendChild(div);
+      });
+    }
   })
   .catch(err => {
     newsList.innerHTML = '<p>Failed to load news.</p>';
